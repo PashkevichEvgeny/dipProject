6b78a65520d9f5e067192599d00a1b094fba041c
PashkevichEvgeny <pash.ev@ya.ru>
2024-05-01 15:35:46 +0800

    temporarily changed root urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("doggy/", include("doggy.urls")),
+    path("", include("doggy.urls")),
     path('admin/', admin.site.urls),
 ]
