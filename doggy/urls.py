7848b69b77302305e1be926d2c42b66cf0e1009b
PashkevichEvgeny <pash.ev@ya.ru>
2024-05-03 18:23:17 +0800

    add views for list and detail of animals

--- doggy/urls.py
+++ doggy/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'doggy'
+
+
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.AnimalListView.as_view(), name="index"),
+    path("<int:pk>/", views.AnimalDetailView.as_view(), name="detail"),
 ]
--- doggy/views.py
+++ doggy/views.py
@@ -1,8 +1,23 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from .models import Animal
+
+template_name = 'doggy/index.html'
 
 
-def index(request):
-    return HttpResponse('Start Page')
+def index(request: HttpRequest) -> HttpResponse:
+    return render(
+        request,
+        template_name=template_name,
+    )
+
+
+class AnimalListView(ListView):
+    template_name = template_name
+    queryset = Animal.objects.all()
+
+
+class AnimalDetailView(DetailView):
+    model = Animal
